6c4dc8cd1c242ea872709cb2323ee459a55bf26c
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2025-10-10 16:35:41 +0000

    Final

--- src/paginas/CriarRepositorio/index.js
+++ src/paginas/CriarRepositorio/index.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import estilos from './estilos';
+import { criarRepositorioDoUsusario } from '../../servicos/requisicoes/repositorios';
 
 export default function CriarRepositorio({ route, navigation }) {
     const [nome, setNome] = useState('');
     const [data, setData] = useState('');
+
+    async function criar() {
+        const resultado = await criarRepositorioDoUsusario
+        (
+            route.params.id,
+            nome,
+            data,
+        )
+        console.log(route.parms.id)
+
+        if(resultado === "sucesso"){
+            Alert.alert('Repositorio Criado')
+            navigation.goBack();
+        }else{
+            Alert.alert('Erro ao criar repositorio')
+        }
+        
+
+    }
 
     return (
         <View style={estilos.container}>
@@ -18,7 +38,9 @@ export default function CriarRepositorio({ route, navigation }) {
                 autoCapitalize="none"
                 style={estilos.entrada}
             />
-            <TouchableOpacity style={estilos.botao}>
+            <TouchableOpacity style={estilos.botao}
+                onPress={criar}
+            >
                 <Text style={estilos.textoBotao}>
                     Criar
                 </Text>
